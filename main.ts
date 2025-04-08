8fbc6182b15d85dbf8dd04fe718cb063227f99fd
anekggomes <00001098808472sp@al.educacao.sp.gov.br>
2025-04-08 09:18:48 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("a =")
+    basic.showNumber(Math.sqrt(b ** 2 + c ** 2))
+})
+let c = 0
+let b = 0
+basic.showString("a")
+basic.pause(100)
+basic.showIcon(IconNames.LeftTriangle)
+b = 3
+c = 4
+basic.showString("b e c")
